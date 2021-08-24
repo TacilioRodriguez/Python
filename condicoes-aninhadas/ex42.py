2540791469824f775e7c1177285969c989f9e65b
Tacilio Rodriguez <taciliorodriguez@gmail.com>
2021-08-24 17:19:57 -0300

    Jogo pedra papel tesoura

--- condicoes-aninhadas/ex42.py
+++ condicoes-aninhadas/ex42.py
@@ -4,3 +4,17 @@ será formado.
 - Isosceles : dois lados iguais
 - Escaleno: Todos os lados diferentes
 """
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro Segmento: '))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR um triângulo', end=' ')
+    if r1 == r2 == r3:
+        print('Equilatero')
+    elif r1 != r2 != r3 != r1:
+        print('Escaleno')
+    else:
+        print('Isosceles')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo')
