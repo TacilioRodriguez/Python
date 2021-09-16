25c006911183570664b5baed51cd651d688fc930
Tacilio Rodriguez <taciliorodriguez@gmail.com>
2021-09-15 22:03:30 -0300

    Teste de Funções

--- estrutura-repeticao-while/ex068.py
+++ estrutura-repeticao-while/ex068.py
@@ -3,3 +3,11 @@ Faça um programa que jogue par ou impar com o computador. O jogo só será inte
 Mostrando o total de vitorias consecutivas que ele conquistou no final do jogo.
 """
 
+def TemLetraU():
+    frase = str(input('Digite uma Frase: ')).upper()
+    if 'U' in frase:
+        print('Tem letra U')
+    else:
+        print('Não tem Letra U')
+
+TemLetraU()
