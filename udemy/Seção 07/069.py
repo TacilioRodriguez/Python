aaa089b3d7420e404e35a695541a98b041c79f6f
Tacilio Rodriguez <taciliorodriguez@gmail.com>
2022-08-20 19:59:29 -0300

    Atualização do Jogo da Forca
    Atualização do Jogo da Forca, e inclusão de novas palavras e uso de modulos

--- udemy/Seção 07/069.py
+++ udemy/Seção 07/069.py
@@ -1,33 +1,49 @@
-# Jogo de acertar palavra
+# Jogo da Forca
 
-#Step 1
-
-word_list = ["ardvark", "baboon", "camel"]
-# word_list = ["ta", 'teste']
-
+from hangman_words import word_list
+from hangman_art import logo, stages
 from random import choice
+
+
+def espaco(qtd):
+    print(qtd*'=')
+
+
+print(logo)
+espaco(50)
+print('Bem vindo ao jogo da forca, você terá 6 chances para acertar... ')
+
 
 palavra_escolhida = choice(word_list)
 tamanho_palavra = len(palavra_escolhida)
-print(palavra_escolhida)
+print(f'A Palavra sorteada foi ==>> {palavra_escolhida}')
 
 display = []
 for traco in range(tamanho_palavra):
     display.append('_')
-print(display)
+# print(display)
 
+lives = 6
+game_over = False
 
-tamanho = 0 #Variavel iteravel/incremental
-
-# Enquanto a quantidade de palpites for menor que o tamanho da palavra e existir espaço vazio na lista
-while tamanho < tamanho_palavra and '_' in display:
-    palpite = str(input('Digite uma Letra: '))
+# Enquanto game_over for igual a False, faça
+while game_over == False:
+    espaco(50)
+    palpite = str(input('Digite uma Letra: ')).lower()
     for posicao in range(tamanho_palavra):
         letra = palavra_escolhida[posicao]
         if letra == palpite:
             display[posicao] = letra
-    tamanho += 1  # incremento da quantidade de palpites
-if '_' in display:
-    print('Não foi dessa vez')
-else:
-    print(f'Você acertou: {display}')
+    print(display)
+    if palpite not in palavra_escolhida:
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            game_over = True
+            print('Enforcado')
+    elif '_' not in display:
+        print(f'Você acertou: {display}')
+        game_over = True
+
+
+
