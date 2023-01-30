265dd4abe2ffb9b8673b0fecbcf0011022cdf24e
Tacilio Rodriguez <taciliorodriguez@gmail.com>
2023-01-30 16:01:29 -0300

    Organizando em pastas
    Organizando em pastas corretas os exercicios de lista dividos em duas partes

--- Listas/Listas Part 1/ex0078.py
+++ Listas/Listas Part 1/ex0078.py
@@ -1,13 +1,11 @@
 
-
-
 lanche = ['teste1', 'teste2', 'teste3']
 
 lanche[2] = 'picole'
 print(lanche[2])
 
-lanche.append('coockie') #adicionar o item a lista
-lanche.insert(0,'dog')  #adicionar em qualquer posição da lista
+lanche.append('coockie')  # adicionar o item a lista
+lanche.insert(0 ,'dog')  # adicionar em qualquer posição da lista
 print(lanche)
 
 # Apagar elementos da lista
